6609ed40e77c06ac4c158404072fd4a65ce33295
Oskar <omai225@gmail.com>
2020-12-13 01:16:58 -0500

    added dyanmic urls for ticket details

--- src/tickets/urls.py
+++ src/tickets/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name='tickets'
 urlpatterns = [
 	path('', views.index, name="index"),
+	path('<int:ticket_id>', views.details,name="details"),
 ]
--- src/tickets/views.py
+++ src/tickets/views.py
@@ -8,6 +8,7 @@ from .forms import *
 import datetime
 
 # Create your views here.
+@login_required(login_url='authenticate:login')
 def index(request):
 	ticket_list = Ticket.objects.all()
 	ticket_filter = TicketFilter(request.GET,queryset=ticket_list)
@@ -17,3 +18,16 @@ def index(request):
 		'ticket_filter': ticket_filter,
 	}
 	return render(request, "tickets/index.html",context)
+
+@login_required(login_url='authenticate:login')
+def details(request,ticket_id):
+	try:
+
+		ticket = Ticket.objects.get(pk=ticket_id)#find ticket object we got from link
+		context = {
+			'ticket':ticket
+		}
+		return render(request, "tickets/details.html",context)
+	except:
+		messages.error(request,"TICKET DOES NOT EXIST")
+		return redirect("tickets:index")
