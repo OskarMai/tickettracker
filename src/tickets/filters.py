f37de7751a709f11b128deb2edaf9dcae180f2bb
Oskar <omai225@gmail.com>
2020-12-18 21:21:39 -0500

    fixed ticket index search filter. fieldnames for foreignkeys must be specified

--- src/tickets/filters.py
+++ src/tickets/filters.py
@@ -5,8 +5,8 @@ from authenticate.models import *
 
 class TicketFilter(django_filters.FilterSet):
 	ID = CharFilter(field_name="id", lookup_expr='icontains',label='Ticket #')
-	Assigned_Project = CharFilter(field_name="project", lookup_expr='icontains',label='project name')
-	Submitter = CharFilter(field_name="submitter",lookup_expr='icontains',label='submitted by')
+	Assigned_Project = CharFilter(field_name="project__name", lookup_expr='icontains',label='project name')
+	Submitter = CharFilter(field_name="submitter__user_name",lookup_expr='icontains',label='submitted by')
 	Priority = CharFilter(field_name='priority',lookup_expr='icontains',label='priority')
 	Status = CharFilter(field_name='status',lookup_expr='icontains',label='status')
 	class Meta:
