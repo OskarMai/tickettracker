b1252979a75e3b07513ac1fd06b8c2c73a43d46f
Oskar <omai225@gmail.com>
2020-12-23 04:48:07 -0500

    added bootstrap cards to autentication pages

--- src/user_profile/views.py
+++ src/user_profile/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render,redirect
-from .models import *
 from .forms import *
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
