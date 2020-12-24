6263eeb7e3a0d40f9ef9756052df40c12f260721
Oskar <omai225@gmail.com>
2020-12-24 01:20:08 -0500

    styled projects index with bootstrap cards

--- src/projects/filters.py
+++ src/projects/filters.py
@@ -1,18 +1,35 @@
 import django_filters
-from django_filters import CharFilter
+from django import forms
+from django_filters import CharFilter, ModelChoiceFilter
 from .models import *
 from authenticate.models import *
 
 class ProjectFilter(django_filters.FilterSet):
-	name = CharFilter(field_name="name", lookup_expr='icontains')
-	description = CharFilter(field_name="description", lookup_expr='icontains')
+	submitter = ModelChoiceFilter(field_name="submitter", queryset=NewUser.objects.all(),widget=forms.Select(
+		attrs={
+			'class':'form-control col-sm-12'
+		}))
+	name = CharFilter(field_name="name", lookup_expr='icontains',widget=forms.TextInput(
+		attrs={
+			'class':'form-control col-sm-12'
+		}))
+	description = CharFilter(field_name="description", lookup_expr='icontains',widget=forms.TextInput(
+		attrs={
+			'class':'form-control col-sm-12'
+		}))
 	class Meta:
 		model = Project
 		fields = '__all__'
 
 class UserFilter(django_filters.FilterSet):
-	username = CharFilter(field_name="user_name", lookup_expr="icontains")
-	email = CharFilter(field_name="email", lookup_expr="icontains")
+	username = CharFilter(field_name="user_name", lookup_expr="icontains",widget=forms.Select(
+		attrs={
+			'class':'form-control'
+		}))
+	email = CharFilter(field_name="email", lookup_expr="icontains",widget=forms.TextInput(
+		attrs={
+			'class':'form-control'
+		}))
 	class Meta:
 		model: NewUser
 		fields = '__all__'
--- src/projects/forms.py
+++ src/projects/forms.py
@@ -5,20 +5,30 @@ from authenticate.models import NewUser
 
 class ProjectAddForm(forms.Form):
 
-	name = forms.CharField(label='name', max_length = 64)
-	description = forms.CharField(label = 'description', max_length=64)
+	name = forms.CharField(label='Name', max_length = 64, widget=forms.TextInput(
+		attrs={
+			'class':'form-control col-sm-3',
+		}))
+	description = forms.CharField(label = 'Description', max_length=1000, widget=forms.Textarea(
+		attrs={
+			'class': 'form-control',
+			'rows': '5',
+		}))
 
 class ProjectDeleteForm(forms.Form):
 	project = forms.ModelChoiceField(queryset=Project.objects.all(), label=None)
-
+	project.widget.attrs['class'] = 'form-control'
 class AssignProjectForm(forms.Form):
 	actionChoices = (
 		('0',u'Add'),
 		('1',u'Remove')
 		)
 	project = forms.ModelChoiceField(queryset= Project.objects.all(), label=None)
+	project.widget.attrs['class'] = 'form-control'
 	member = forms.ModelChoiceField(queryset= NewUser.objects.filter(groups__name__in=['admin','developer']), label=None)
+	member.widget.attrs['class']='form-control'
 	action = forms.ChoiceField(required = True, choices= actionChoices)
+	action.widget.attrs['class']='form-control'
 
 	class Meta:
 		model = ProjectMember
