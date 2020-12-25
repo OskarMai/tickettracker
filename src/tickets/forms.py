c1d40157c1d57dc0748d2de7c2d6f900fd146f5d
Oskar <omai225@gmail.com>
2020-12-24 21:52:52 -0500

    finished basic styling

--- src/tickets/forms.py
+++ src/tickets/forms.py
@@ -5,7 +5,10 @@ from .models import *
 
 class SubmitTicketForm(forms.Form):
 	project = forms.ModelChoiceField(queryset= Project.objects.all(), label=None)
-	description = forms.CharField(max_length=1000, label="Describe the issue")
+	description = forms.CharField(max_length=1000, label="Describe the issue",widget=forms.Textarea(
+		attrs={
+			'rows':'4'
+		}))
 	project.widget.attrs['class']='form-control col-sm-12'
 	description.widget.attrs['class']='form-control col-sm-12'
 	class Meta:
@@ -16,10 +19,15 @@ class EditTicketForm(forms.ModelForm): #creating a master ticket form with all f
 	class Meta:
 		model = Ticket
 		fields = '__all__'
-
 class FileForm(forms.Form):
 	file = forms.FileField(label='Select a file',)
-	description = forms.CharField(max_length=100)
+	description = forms.CharField(max_length=100,widget=forms.TextInput(
+		attrs={
+			'class':'form-control col-sm-12'
+		}))
 
 class CommentForm(forms.Form):
-	message = forms.CharField(max_length = 100)
+	message = forms.CharField(max_length = 100,widget=forms.TextInput(
+		attrs={
+			'class':'form-control col-sm-12'
+		}))
